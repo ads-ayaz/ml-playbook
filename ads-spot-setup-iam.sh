92866063754b6ada0dd191a88ee3bb74973d73c0
Ayaz Somani <ayaz.somani@aluance.com>
2020-09-05 14:46:28 +0000

    Added spot fleet role creation.

--- ads-spot-setup-iam.sh
+++ ads-spot-setup-iam.sh
@@ -2,15 +2,23 @@
 
 # ads-spot-setup-iam.sh
 #
+# Description:
+# Script creates necessary IAM roles for automated ML spot fleet training.
+#
+# Assumptions:
+# This is being run from an EC2 instance that already has the aws CLI configured.
+#
 #################################
 
 
 # Script defaults
 ADS_AWS_ACCOUNT_ID=512704425807
 ADS_AWS_REGION=ca-central-1
+ADS_IAM_ROLE_SPOT_FLEET=ADS-ML-spot-fleet-role
 ADS_IAM_ROLE_SPOT_TRAINING=ADS-ML-spot-training-role
 ADS_IAM_POLICY_NAME_SPOT_TRAINING=ADS-ML-spot-training-permissions
 ADS_IAM_POLICY_FILE_SPOT_TRAINING=ads-ml-spot-training-permissions.json
+
 
 ADS_PATH_HOME=/home/ubuntu/
 ADS_PATH_MOUNT=/ads-ml/
@@ -21,7 +29,7 @@ ADS_PATH_LOG=${ADS_PATH_MOUNT}logs/
 ADS_VOLUME_DATASET_NAME=ml-spot-training-data
 ADS_VOLUME_DATASET_SIZE=100
 
-# Create a new role for spot ML training
+# Create a new role for spot ML training instance
 aws iam create-role \
     --role-name ${ADS_IAM_ROLE_SPOT_TRAINING} \
     --assume-role-policy-document '{"Version":"2012-10-17","Statement":[{"Sid":"","Effect":"Allow","Principal":{"Service":"ec2.amazonaws.com"},"Action":"sts:AssumeRole"}]}'
@@ -37,3 +45,12 @@ aws iam attach-role-policy \
     --role-name ${ADS_IAM_ROLE_SPOT_TRAINING}
     
 
+# Create a new role for spot ML training fleet
+aws iam create-role \
+     --role-name ${ADS_IAM_ROLE_SPOT_FLEET} \
+     --assume-role-policy-document '{"Version":"2012-10-17","Statement":[{"Sid":"","Effect":"Allow","Principal":{"Service":"spotfleet.amazonaws.com"},"Action":"sts:AssumeRole"}]}'
+
+# Attach AmazonEC2SpotFleetTaggingRole to the fleet policy
+aws iam attach-role-policy \
+     --policy-arn arn:aws:iam::aws:policy/service-role/AmazonEC2SpotFleetTaggingRole --role-name ${ADS_IAM_ROLE_SPOT_FLEET}
+     
