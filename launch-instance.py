abd1095e734b08e2a35fde26ce365e8b7c88b5c3
Ayaz Somani <ayaz@aluance.com>
2020-09-05 10:49:43 -0400

    Various upgrades to this script.

--- launch-instance.py
+++ launch-instance.py
@@ -1,17 +1,29 @@
 #!/usr/bin/env python
 
-ATTEMPTS_MAX = 40
+ATTEMPTS_MAX = None #400
+ATTEMPTS_DURATION = None #6
 DELAY_MIN = 60
 DELAY_MAX = 120
 
 
 import boto3
 from botocore.exceptions import ClientError
-from time import sleep
 import random
+import time
+from time import sleep
+
+# Set maximum time for attempts. Default is 1 hour
+t_max = time.time() + 3600
+if ATTEMPTS_DURATION is not None :
+  t_max = time.time() + (3600 * ATTEMPTS_DURATION)
+
+# Set maximum number of attempts. Default is 10
+attempts_max = 10
+if ATTEMPTS_MAX is not None :
+  attempts_max = ATTEMPTS_MAX
+
 
 client = boto3.client('ec2')
-
 
 launch_template = {
   'LaunchTemplateId': 'lt-06def67836adb9459',
@@ -19,17 +31,22 @@ launch_template = {
   'Version': '2'
 }
 
+placement = {
+  'AvailabilityZone': 'ca-central-1b'
+}
+
 response='\nUnable to launch instance.'
 num_attempts = 0
 
-while num_attempts < ATTEMPTS_MAX :
+while (num_attempts < attempts_max) and (time.time() < t_max) :
   try :
     num_attempts += 1
-    print("Attempt %i of %i:" % (num_attempts, ATTEMPTS_MAX))
+    print("Attempt %i of %i:" % (num_attempts, attempts_max))
 
     response = client.run_instances( \
-      # InstanceType='m1.large', \
+      InstanceType='g4dn.8xlarge', \
       LaunchTemplate=launch_template, \
+      # Placement=placement, \
       MinCount=1, \
       MaxCount=1 )
 
@@ -37,9 +54,10 @@ while num_attempts < ATTEMPTS_MAX :
 
   except ClientError as e:
     print("\t" + str(e))
-    delay = random.randint(DELAY_MIN, DELAY_MAX)
-    print("\tRetry in %i seconds" % delay)
-    sleep(delay)
+    if num_attempts < attempts_max :
+      delay = random.randint(DELAY_MIN, DELAY_MAX)
+      print("\tRetry in %i seconds" % delay)
+      sleep(delay)
 
 print(response)
 
